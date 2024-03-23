460c201f28bc73f49f6c9697adbdcf28d1139d41
Kadir Jun Ayhan <88925612+kjayhan@users.noreply.github.com>
2024-03-23 16:45:26 +0900

    int students

--- data-raw/iso3c_data.R
+++ data-raw/iso3c_data.R
@@ -1,4 +1,4 @@
-iso3c_data <- readr::read_csv("../best_practices/data/ultimate_iso_kr.csv")
+iso3c_data <- readr::read_csv("../best_practices/data/ultimate_iso_kr.csv", lazy = FALSE)
 
-readr::write_csv(iso3c_data, "data_raw/iso3c_data.csv")
+readr::write_csv(iso3c_data, "data-raw/iso3c_data.csv")
 usethis::use_data(iso3c_data, overwrite = TRUE, internal = FALSE)
