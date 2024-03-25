af4f85428178ec246149cd4a42012e7c3f52f2cd
Kadir Jun Ayhan <88925612+kjayhan@users.noreply.github.com>
2024-03-26 01:32:07 +0900

    kdiplo-package.R added. Title changed.

---
+++ R/kdiplo-package.R
@@ -0,0 +1,16 @@
+#' Generate customized data frames containing various data on Korean
+#' diplomacy and foreign policy.
+#'
+#' The \code{kdiplo} package contains functions for generating various data
+#' on Korean diplomacy and foreign policy
+#'
+#' @name kdiplo-package
+#' @aliases kdiplo-package
+#' @title kdiplo: A One-Stop Public Repository for Datasets on Korean Diplomacy
+#' @author Kadir Jun Ayhan \email{k@ayhan.phd}
+#' @references
+#'    \url{https://ayhan.phd/}
+#'    \url{https://github.com/kjayhan/kdiplo}
+#' @docType _PACKAGE
+#' @keywords package
+NULL
