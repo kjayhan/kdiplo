60d35c7b7d63d6b0cd23fda03eaa72ee551a5e11
Kadir Jun Ayhan <88925612+kjayhan@users.noreply.github.com>
2025-06-16 20:44:21 -0400

    testthat

--- tests/testthat/test-testthat.R
+++ tests/testthat/test-testthat.R
@@ -6,3 +6,8 @@ library(stats)
 library(readr)
 
 test_check("kdiplo")
+test_that("Correct conversion of Korean country names to iso3c codes", {
+  df <- tibble(country_kr = c("중국", "미국", "일본"))  # China, USA, Japan
+  result <- iso3c_kr(df, "country_kr")
+  expect_equal(result$iso3c, c("CHN", "USA", "JPN"))
+})
